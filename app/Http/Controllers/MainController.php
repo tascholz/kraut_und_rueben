41b697a7a097317ea09af5dae40eaf7d384e43b9
tascholz <timo.andreas.scholz@gmx.de>
2021-10-19 10:54:10 +0200

    fix: restored env.example feature: addIngredient to index

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Customer;
+use App\Models\Recipe;
+use App\Models\Ingredient;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $ingredients = Ingredient::all();
+        
+        return view('index', [
+                                'ingredients' => $ingredients
+                            ]);
     }
 
     public function showRecipes()
